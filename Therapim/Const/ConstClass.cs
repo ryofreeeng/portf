277965b8f858323a88ad8c296c181906e02133a7
ryo <ryo.therapist@gmail.com>
2024-10-23 23:20:59 +0900

    ログイン後のリダイレクト少し実装

--- Therapim/Const/ConstClass.cs
+++ Therapim/Const/ConstClass.cs
@@ -8,11 +8,11 @@
         /// <summary>
         /// GasAPIのURL（本番用）
         /// </summary>
-        //public const string GAS_API_URL = "https://script.google.com/macros/s/AKfycbyxOuMze4Nw9jEIyOc8pHuMfJ8ShnoAIxFOcV1P9_BJJtpeVXsHnWa8mSo317ScyJbs/exec";
+        public const string GAS_API_URL = "https://script.google.com/macros/s/AKfycbyxOuMze4Nw9jEIyOc8pHuMfJ8ShnoAIxFOcV1P9_BJJtpeVXsHnWa8mSo317ScyJbs/exec";
         /// <summary>
         /// GasAPIのURL（テスト用）
         /// </summary>
-        public const string GAS_API_URL = "https://script.google.com/macros/s/AKfycbxMgMuLvt9xYwN3IHA0z8hsc2PSSfW-3p0RNkTovUNl4ROO6Xgo6pLAPC4NGYXhNt5d/exec";
+        //public const string GAS_API_URL = "https://script.google.com/macros/s/AKfycbxMgMuLvt9xYwN3IHA0z8hsc2PSSfW-3p0RNkTovUNl4ROO6Xgo6pLAPC4NGYXhNt5d/exec";
 
         /// <summary>
         /// LINEログイン時に使用するチャネルID
